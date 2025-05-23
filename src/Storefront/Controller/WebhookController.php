de8f9fac2b39cd4e7900f4267c04e714fe4011f6
solution-25 <info@solution25.com>
2025-05-23 13:31:06 +0200

    Webhooks threeD check

--- src/Storefront/Controller/WebhookController.php
+++ src/Storefront/Controller/WebhookController.php
@@ -11,6 +11,7 @@ use BlueSnap\Service\BlueSnapConfig;
 use BlueSnap\Service\BlueSnapTransactionService;
 use BlueSnap\Service\OrderService;
 use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStateHandler;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,62 +21,66 @@ use Psr\Log\LoggerInterface;
 #[Route(defaults: ['_routeScope' => ['storefront']])]
 class WebhookController
 {
-    private OrderService $orderService;
-    private OrderTransactionStateHandler $transactionStateHandler;
-    private BlueSnapTransactionService $blueSnapTransactionService;
-    private BlueSnapConfig $blueSnapConfig;
-    private LoggerInterface $logger;
+  private OrderService $orderService;
+  private OrderTransactionStateHandler $transactionStateHandler;
+  private BlueSnapTransactionService $blueSnapTransactionService;
+  private BlueSnapConfig $blueSnapConfig;
+  private LoggerInterface $logger;
 
-    public function __construct(
-        OrderService $orderService,
-        OrderTransactionStateHandler $transactionStateHandler,
-        BlueSnapTransactionService $blueSnapTransactionService,
-        BlueSnapConfig $blueSnapConfig,
-        LoggerInterface $logger
-    ) {
-        $this->orderService               = $orderService;
-        $this->transactionStateHandler    = $transactionStateHandler;
-        $this->blueSnapTransactionService = $blueSnapTransactionService;
-        $this->blueSnapConfig             = $blueSnapConfig;
-        $this->logger                     = $logger;
+  public function __construct(
+    OrderService                 $orderService,
+    OrderTransactionStateHandler $transactionStateHandler,
+    BlueSnapTransactionService   $blueSnapTransactionService,
+    BlueSnapConfig               $blueSnapConfig,
+    LoggerInterface              $logger
+  )
+  {
+    $this->orderService = $orderService;
+    $this->transactionStateHandler = $transactionStateHandler;
+    $this->blueSnapTransactionService = $blueSnapTransactionService;
+    $this->blueSnapConfig = $blueSnapConfig;
+    $this->logger = $logger;
+  }
+
+  #[Route(path: '/webhook', name: 'api.webhook', methods: ['POST', 'GET'])]
+  public function webhook(Request $request, SalesChannelContext $context): JsonResponse
+  {
+
+    $rawData = $request->getContent();
+    parse_str($rawData, $params);
+
+    $this->logger->info(json_encode($rawData));
+
+    $transactionType = $params['transactionType'] ?? '';
+    $transactionId = $params['merchantTransactionId'] ?? ''; // Same as orderID
+    $captureReferenceNumber = $params['captureReferenceNumber'] ?? '';
+
+    $enabledThreeD = $this->blueSnapConfig->getConfig('threeDS', $context->getSalesChannelId());
+    if ($enabledThreeD) {
+      $threeD = $params['3DStatus'] ?? '';
+      if ($threeD !== 'AUTHENTICATION_SUCCEEDED') {
+        return new JsonResponse(['status' => false]);
+      }
     }
 
-    #[Route(path: '/webhook', name: 'api.webhook', methods: ['POST', 'GET'])]
-    public function webhook(Request $request, Context $context): JsonResponse
-    {
-        $rawData = $request->getContent();
-        parse_str($rawData, $params);
-
-
-        $this->logger->info(json_encode($rawData));
-
-        $threeD                 = $params['3DStatus']               ?? '';
-        $transactionType        = $params['transactionType']        ?? '';
-        $transactionId          = $params['merchantTransactionId']  ?? ''; // Same as orderID
-        $captureReferenceNumber = $params['captureReferenceNumber'] ?? '';
-
-        if ($threeD !== 'AUTHENTICATION_SUCCEEDED') {
-            return new JsonResponse(['status' => false]);
-        }
-
-        if ($transactionType !== 'CHARGE') {
-            return new JsonResponse(['status' => false]);
-        }
-
-        if (!$transactionId) {
-            return new JsonResponse(['status' => false]);
-        }
-
-        $order       = $this->orderService->getOrderDetailsById($transactionId, $context);
-        $transaction = $order->getTransactions()->first();
-
-        if ($transaction->getPaymentMethod()->getHandlerIdentifier() != LinkPayment::class && $transaction->getPaymentMethod()->getHandlerIdentifier() != HostedCheckout::class) {
-            return new JsonResponse(['status' => false]);
-        }
-
-        $this->transactionStateHandler->paid($transaction->getId(), $context);
-
-        $this->blueSnapTransactionService->updateTransactionStatus($order->getId(), TransactionStatuses::PAID->value, $context, $captureReferenceNumber);
-        return new JsonResponse(['status' => true]);
+    if ($transactionType !== 'CHARGE') {
+      return new JsonResponse(['status' => false]);
     }
+
+    if (!$transactionId) {
+      return new JsonResponse(['status' => false]);
+    }
+
+    $order = $this->orderService->getOrderDetailsById($transactionId, $context->getContext());
+    $transaction = $order->getTransactions()->first();
+
+    if ($transaction->getPaymentMethod()->getHandlerIdentifier() != LinkPayment::class && $transaction->getPaymentMethod()->getHandlerIdentifier() != HostedCheckout::class) {
+      return new JsonResponse(['status' => false]);
+    }
+
+    $this->transactionStateHandler->paid($transaction->getId(), $context->getContext());
+
+    $this->blueSnapTransactionService->updateTransactionStatus($order->getId(), TransactionStatuses::PAID->value, $context, $captureReferenceNumber);
+    return new JsonResponse(['status' => true]);
+  }
 }
